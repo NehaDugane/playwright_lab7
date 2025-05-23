19451fbd5d792a935cd1a86c1ab97b687ff02713
root <root@ip-172-31-3-146.us-east-2.compute.internal>
2025-05-23 11:23:38 +0000

    last commit

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  use: {
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
+  }
+};
+
+
---
+++ tests/todo.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('default ToDo test', async ({ page }) => {
+  await page.goto('https://playwright.dev');
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+
